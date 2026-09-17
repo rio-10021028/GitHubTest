c20b44d3df397975f88cdc2e31a2029956a9007d
吏雄 勝又 <riy10sweet@outlook.jp>
2026-09-17 13:31:47 +0900

    using namespace std を書いたよ。

--- GitHubTest/GitHubTest.cpp
+++ GitHubTest/GitHubTest.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
